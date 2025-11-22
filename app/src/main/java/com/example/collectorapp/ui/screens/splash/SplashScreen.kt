fec90c0d8f6f9f83026bb6b4106533e8e9b8c2a3
ncondeviladev <ncondeviladev@gmailcom>
2025-11-22 19:00:52 +0100

    Añadido pantalla bienvenida y tipografia

---
+++ app/src/main/java/com/example/collectorapp/ui/screens/splash/SplashScreen.kt
@@ -0,0 +1,45 @@
+package com.example.collectorapp.ui.screens.splash
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import com.example.collectorapp.ui.navigation.Rutas
+import kotlinx.coroutines.delay
+
+@Composable
+fun SplashScreen(navController: NavController) {
+    // Usamos un LaunchedEffect para la navegación después de un tiempo.
+    // La clave `true` hace que solo se ejecute una vez.
+    LaunchedEffect(key1 = true) {
+        delay(2000) // Esperamos 2 segundos
+        navController.navigate(Rutas.LISTA_COLECCIONESS) {
+            // Limpiamos la pila de navegación para que el usuario no pueda volver atrás.
+            popUpTo(Rutas.SPLASH) {
+                inclusive = true
+            }
+        }
+    }
+
+    // El contenido de la pantalla
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.primaryContainer),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Collector App",
+            style = MaterialTheme.typography.headlineLarge,
+            color = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    }
+}
